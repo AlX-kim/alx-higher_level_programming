6da1521ea1c99ad460fe624c8d81e7f990d59422
AlX-kim <ekipmurkor@gmail.com>
2022-05-30 06:37:14 -0700

    python scripts

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
+      language that combines remarkable power with very clear syntax"
 Newstr = str[39:67] + str[107:112] + str[:6]
-print(\Newstr)
+print(Newstr)
