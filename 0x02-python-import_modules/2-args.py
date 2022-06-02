c176900830bb38518e981b722ffb7840f97a6155
AlX-kim <ekipmurkor@gmail.com>
2022-06-02 05:44:04 -0700

    python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
-    return
+        return
     else:
         if n == 1:
             print("{:d} argument:".format(n))
